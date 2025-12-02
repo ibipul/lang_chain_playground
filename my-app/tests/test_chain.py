9f17a08997f757cea89f7a596ee58afcdf939c10
ibipul <islam.bipul@gmail.com>
2025-12-02 23:43:13 +0530

    Added sample runnable cases wrapped in pytest to run with langchain client

--- my-app/tests/test_chain.py
+++ my-app/tests/test_chain.py
@@ -1,6 +1,22 @@
+import langsmith
 from my_app.chain import chain 
 
-def test_chain():
-    print(
-        chain.invoke({"text": "hello there, do you have any gold for me?"})
+from datetime import datetime
+
+def test_chain_basic():
+    input_data = {"text": "Hello, how are you today, friend?"}
+    result = chain.invoke(input_data)
+    print(result)
+
+def test_chain_langsmith():
+    client = langsmith.Client()
+    date_int = int(datetime.now().timestamp())
+    chain_result = client.run_on_dataset(
+        dataset_name="pirate-dataset-echo-chain",
+        llm_or_chain_factory=chain,
+        project_name=f"pirate-dataset-echo-chain-test-{date_int}",
+        concurrency_level=2,
+        verbose=True
     )
+        
+    
